44707418b2a3871a3783eb180f6e11a25f367732
liangzichao <13552167915@qq.com>
2024-12-12 23:21:02 +0800

    睡觉去了

---
+++ learncmake/v1/build/CMakeFiles/app.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/root/work/learncmake/add.cpp" "/root/work/learncmake/build/CMakeFiles/app.dir/add.cpp.o"
+  "/root/work/learncmake/div.cpp" "/root/work/learncmake/build/CMakeFiles/app.dir/div.cpp.o"
+  "/root/work/learncmake/main.cpp" "/root/work/learncmake/build/CMakeFiles/app.dir/main.cpp.o"
+  "/root/work/learncmake/mult.cpp" "/root/work/learncmake/build/CMakeFiles/app.dir/mult.cpp.o"
+  "/root/work/learncmake/sub.cpp" "/root/work/learncmake/build/CMakeFiles/app.dir/sub.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ learncmake/v2/build/CMakeFiles/calc.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/root/work/learncmake/v2/src/add.cpp" "/root/work/learncmake/v2/build/CMakeFiles/calc.dir/src/add.cpp.o"
+  "/root/work/learncmake/v2/src/div.cpp" "/root/work/learncmake/v2/build/CMakeFiles/calc.dir/src/div.cpp.o"
+  "/root/work/learncmake/v2/src/mult.cpp" "/root/work/learncmake/v2/build/CMakeFiles/calc.dir/src/mult.cpp.o"
+  "/root/work/learncmake/v2/src/sub.cpp" "/root/work/learncmake/v2/build/CMakeFiles/calc.dir/src/sub.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")

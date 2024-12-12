0bdd6704187fb30439600883f8fafa951eaeba80
liangzichao <13552167915@qq.com>
2024-12-12 22:03:07 +0800

    first_dir_commit

---
+++ learncmake/build/CMakeFiles/app.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/app.dir/main.cpp.o"
+  "CMakeFiles/app.dir/add.cpp.o"
+  "CMakeFiles/app.dir/sub.cpp.o"
+  "CMakeFiles/app.dir/mult.cpp.o"
+  "CMakeFiles/app.dir/div.cpp.o"
+  "../bin1/app.pdb"
+  "../bin1/app"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
